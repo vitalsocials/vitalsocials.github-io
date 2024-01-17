7ba683ffeb5272993d9253061d46f1551a5dcc0d
TrueWebMagic <nicholaspiech@gmail.com>
2024-01-17 21:53:16 +0100

    Responsive update #5

--- sticky-menu.js
+++ sticky-menu.js
@@ -2,17 +2,21 @@ var navbar = document.querySelector('.hero-section header');
 
 var sticky = navbar.offsetTop;
 
-window.onscroll = function() {
+if (window.innerWidth <= 950) {
 
-    if (window.pageYOffset > sticky) {
-        navbar.classList.add("sticky")
-        document.body.style.paddingTop = "80px";
-        document.querySelector('.hamburger-bg').style.zIndex = "-1";
-        document.querySelector('.hamburger-bg').style.borderRadius = "0";
-    } else {
-        navbar.classList.remove("sticky");
-        document.body.style.paddingTop = "0";
-        document.querySelector('.hamburger-bg').style.borderRadius = "0 0 0 100%";
-    }
+    window.onscroll = function() {
+    
+        if (window.pageYOffset > sticky) {
+            navbar.classList.add("sticky")
+            document.body.style.paddingTop = "80px";
+            document.querySelector('.hamburger-bg').style.zIndex = "-1";
+            document.querySelector('.hamburger-bg').style.borderRadius = "0";
+        } else {
+            navbar.classList.remove("sticky");
+            document.body.style.paddingTop = "0";
+            document.querySelector('.hamburger-bg').style.borderRadius = "0 0 0 100%";
+        }
+    
+    };
 
 };
