0530d4583eb467073053224c7b5a25bba755488e
TrueWebMagic <nicholaspiech@gmail.com>
2024-01-18 16:11:16 +0100

    Responsive update #6

--- sticky-menu.js
+++ sticky-menu.js
@@ -2,21 +2,62 @@ var navbar = document.querySelector('.hero-section header');
 
 var sticky = navbar.offsetTop;
 
-if (window.innerWidth <= 950) {
+function setSticky() {
 
-    window.onscroll = function() {
-    
+    if (window.innerWidth <= 1110) {
+
+        // console.log("innerWidth");
+
         if (window.pageYOffset > sticky) {
+            // console.log("add sticky");
             navbar.classList.add("sticky")
             document.body.style.paddingTop = "80px";
             document.querySelector('.hamburger-bg').style.zIndex = "-1";
             document.querySelector('.hamburger-bg').style.borderRadius = "0";
         } else {
+            // console.log("remove sticky");
             navbar.classList.remove("sticky");
             document.body.style.paddingTop = "0";
             document.querySelector('.hamburger-bg').style.borderRadius = "0 0 0 100%";
         }
-    
+
+    }
+    else {
+        // console.log("else");
+        if (navbar.classList.contains("sticky")) {
+            // console.log("else remove sticky");
+            navbar.classList.remove("sticky");
+            document.body.paddingTop = "0";
+        }
     };
 
+}
+
+window.onscroll = function() {
+    if (window.innerWidth <= 1110) {
+
+        // console.log("innerWidth");
+
+        if (window.pageYOffset > sticky) {
+            // console.log("add sticky");
+            navbar.classList.add("sticky")
+            document.body.style.paddingTop = "80px";
+            document.querySelector('.hamburger-bg').style.zIndex = "-1";
+            document.querySelector('.hamburger-bg').style.borderRadius = "0";
+        } else {
+            // console.log("remove sticky");
+            navbar.classList.remove("sticky");
+            document.body.style.paddingTop = "0";
+            document.querySelector('.hamburger-bg').style.borderRadius = "0 0 0 100%";
+        }
+
+    }
+    else {
+        // console.log("else");
+        if (navbar.classList.contains("sticky")) {
+            // console.log("else remove sticky");
+            navbar.classList.remove("sticky");
+            document.body.paddingTop = "0";
+        }
+    };
 };
